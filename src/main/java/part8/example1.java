777ad379b7106b9d7a7f5747c9b636f30715fc30
InryeolChoi <dlsfuf0316@gmail.com>
2026-01-07 00:01:22 +0900

    update 8.1

---
+++ src/main/java/part8/example1.java
@@ -0,0 +1,14 @@
+package part8;
+
+import java.util.*;
+
+public class example1 {
+    public static void main(String[] args) {
+        // old version
+        final List<String> roles = new ArrayList<>();
+        roles.add("USER");
+        roles.add("ADMIN");
+
+        // roles = new ArrayList<>();
+    }
+}
