e8616aff96785048f3812b2f42224acdc17fe568
InryeolChoi <dlsfuf0316@gmail.com>
2026-03-01 00:12:07 +0900

    finish chap17

--- src/main/java/part17/Example3/Main.java
+++ src/main/java/part17/Example3/Main.java
@@ -1,8 +1,21 @@
 package part17.Example3;
 
+import io.reactivex.rxjava3.core.Observable;
+
+import static part17.Example3.TempObservable.getCelsiusTemperatures;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        new ProxyServer(8080).start();
-        System.out.println("🌍 Open: http://localhost:8080");
+
+    public static void main(String[] args) {
+        Observable<TempInfo> observable = getCelsiusTemperatures("New York", "Chicago", "San Francisco");
+        observable.subscribe(new TempObserver());
+
+        try {
+            Thread.sleep(10000L);
+        }
+        catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
---
+++ src/main/java/part17/Example3/TempObservable.java
@@ -0,0 +1,38 @@
+package part17.Example3;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.core.Observable;
+public class TempObservable {
+
+    public static Observable<TempInfo> getTemperature(String town) {
+        return Observable.create(emitter ->
+            Observable.interval(1, TimeUnit.SECONDS)
+                .subscribe(i -> {
+                    if (!emitter.isDisposed()) {
+                        if (i >= 5) {emitter.onComplete();}
+                        else {
+                            try {emitter.onNext(TempInfo.fetch(town));}
+                            catch (Exception e) {emitter.onError(e);}
+                        }
+                    }
+                }
+            )
+        );
+    }
+
+    public static Observable<TempInfo> getCelsiusTemperature(String town) {
+        return getTemperature(town).map(temp ->
+                new TempInfo(temp.getTown(), (temp.getTemp() - 32) * 5 / 9));
+    }
+
+    public static Observable<TempInfo> getCelsiusTemperatures(String... towns) {
+        return Observable.merge(Arrays.stream(towns)
+                .map(TempObservable::getCelsiusTemperature)
+                .collect(toList()));
+    }
+
+}
